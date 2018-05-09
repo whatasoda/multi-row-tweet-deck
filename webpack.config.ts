d00ff337f84e57b128c5d852c6beaccc67d4440a
whatasoda <a16dc107@dhw.ac.jp>
2018-05-09 23:25:00 +0900

    [fixed] build時のjsonファイルの生成タイミングのズレによるバグを修正 tsのbuildと同時にjsonが出力されていて、正しく情報が渡されないことがあった webpack.configの見た目を整理した

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,164 +1,161 @@
-import * as webpack from "webpack"
-import * as path from "path"
+import * as Webpack from 'webpack'
+import * as path from 'path'
 import JSONProxy from './JSONProxy'
+import * as fs from 'fs-extra'
 
-const raw     = process.env.NODE_ENV
-const ENV     = raw ? raw.split(',') : []
-const isDev   = !ENV.includes('production')
-const isTrial = ENV.includes('trial')
-
-type Reload = <T>(path: string) => { default: T }
-const reload              = require('require-nocache')(module) as Reload
 const CopyWebpackPlugin   = require('copy-webpack-plugin')
 const ZipPlugin           = require('zip-webpack-plugin')
 const GenerateJsonPlugin  = require('generate-json-webpack-plugin')
 const PreBuildWebpack     = require('pre-build-webpack')
 
-import genManifest  from './manifest'
-import genAppConfig from './appConfig'
-import GenMessage   from './manifest/locales/base'
+type Reload     = <T>(path: string) => { default: T }
+const reload    = require('require-nocache')(module) as Reload
+const genLoader = <T>(path: string) => (() => reload<T>(path).default)
 
-const locales = {
-  ja: true,
-  en: true
-}
-const manifest  = () => reload<typeof genManifest>('./manifest').default
-const appConfig = () => reload<typeof genAppConfig>('./appConfig').default
-
-type LocaleMap<T>         = { [L in keyof typeof locales]: T}
-type GenMessageReloaders  = LocaleMap<() => GenMessage>
-type Messages             = LocaleMap<ReturnType<GenMessage>>
-
-const msgReloaders: GenMessageReloaders = {} as GenMessageReloaders
-const messages: Messages = {} as Messages
-const MESSAGES: Messages = {} as Messages
-for (const _locale in locales) {
-  const locale = _locale as keyof Messages
-  const reloadGen =
-    () => reload<GenMessage>(`./manifest/locales/${locale}`).default
-  msgReloaders[locale] = reloadGen
-  messages[locale] = reloadGen()(isTrial)
-  MESSAGES[locale] = JSONProxy(messages, locale)
-}
-
-const json = {
-  manifest  : manifest()(),
-  appConfig : appConfig()(isTrial),
-}
-
-const MANIFEST  = JSONProxy(json, 'manifest')
-const APPCONFIG = JSONProxy(json, 'appConfig')
-
-const { version } = APPCONFIG
-
-const plugins: webpack.Plugin[] = [
-  new PreBuildWebpack(function () {
-    reload('./package.json')
-    json.manifest   = manifest()()
-    json.appConfig  = appConfig()(isTrial)
-  }),
-  new GenerateJsonPlugin('manifest.json', MANIFEST),
-  new GenerateJsonPlugin('../src/appConfig.json', APPCONFIG),
-]
-
-if (!isDev) {
-  for (const locale in MESSAGES) {
-    const MESSAGE = MESSAGES[locale as keyof Messages]
-    plugins.push(
-      new GenerateJsonPlugin(`_locales/${locale}/messages.json`, MESSAGE)
-    )
-  }
-  plugins.push(
-    new CopyWebpackPlugin([
-      { from: 'src/icons/', to: 'icons/', ignore: [ '*.ai', '*.svg' ] }
-    ]),
-    new ZipPlugin({
-      path        : path.join(__dirname, 'archive'),
-      filename    : isTrial ? `trial_v${version}` : `v${version}`,
-      pathPrefix  : 'dist'
-    })
+function emitJSON (
+  path: string,
+  obj: any
+) {
+  fs.outputJson(
+    path,
+    obj,
+    {},
+    err => console.log(err || `[emitted] ${path}`)
   )
 }
 
 
+const raw     = process.env.NODE_ENV
+const ENV     = raw ? raw.split(',') : []
+const isDev   = !ENV.includes('production')
+const isTrial = ENV.includes('trial')
+const LOCALES = {
+  ja: true,
+  en: true
+}
+type Locale   = keyof typeof LOCALES
+const locales = Reflect.ownKeys(LOCALES) as Locale[]
 
-const common = {
-  exclude: /svg/,
+const PLUGINS : Webpack.Plugin[]  = []
+const RULES   : Webpack.Rule[]    = []
+const ENTRY   : Webpack.Entry     = {}
+type RESOLVE_MEMBER = 'modules' | 'extensions'
+const RESOLVE : Required<Pick<Webpack.Resolve, RESOLVE_MEMBER>> = {
+  modules   : [],
+  extensions: [],
+}
+const PREBUILD: (() => void)[]    = []
+
+if (isDev) {
+  PLUGINS.push( new PreBuildWebpack(() => {
+    PREBUILD.forEach(prebuild => prebuild())
+  }))
+  PREBUILD.push(() => reload('./package.json'))
 }
 
-const rules: webpack.Rule[] = []
-rules.push({
-  ...common,
+PLUGINS.push( new CopyWebpackPlugin([
+  { from: 'src/icons/', to: 'icons/', ignore: [ '*.ai', '*.svg' ] }
+]))
+
+
+import genManifest  from './manifest'
+const loadManifest  = genLoader<typeof genManifest>('./manifest')
+const manifest  = { default: loadManifest()(isTrial) }
+const MANIFEST  = JSONProxy(manifest,   'default')
+if (isDev) {
+  PLUGINS.push( new GenerateJsonPlugin('manifest.json', MANIFEST) )
+  PREBUILD.push(() => ( manifest.default = loadManifest()(isTrial) ))
+} else {
+  emitJSON('dist/manifest.json', MANIFEST)
+}
+
+
+import genAppConfig from './appConfig'
+const loadAppConfig = genLoader<typeof genAppConfig>('./appConfig')
+const appConfig = { default: loadAppConfig()(isTrial) }
+const APPCONFIG = JSONProxy(appConfig,  'default')
+if (isDev) {
+  PLUGINS.push( new GenerateJsonPlugin('../src/appConfig.json', APPCONFIG) )
+  PREBUILD.push(() => ( appConfig.default = loadAppConfig()(isTrial) ))
+} else {
+  emitJSON('src/appConfig.json', APPCONFIG)
+}
+
+
+import GenMessage from './manifest/locales/base'
+const messages = {} as { [L in Locale]: ReturnType<GenMessage> }
+locales.forEach( locale => {
+  const loadMessage = genLoader<GenMessage>(`./manifest/locales/${locale}`)
+  messages[locale]  = loadMessage()(isTrial)
+  const MESSAGE     = JSONProxy(messages, locale)
+  if (isDev) {
+    PLUGINS.push(
+      new GenerateJsonPlugin(`_locales/${locale}/messages.json`, MESSAGE)
+    )
+    PREBUILD.push(() => ( messages[locale] = loadMessage()(isTrial) ))
+  } else {
+    emitJSON(`dist/_locales/${locale}/messages.json`, MESSAGE)
+  }
+})
+
+const { version } = APPCONFIG
+if (!isDev) {
+  PLUGINS.push( new ZipPlugin({
+    path        : path.join(__dirname, 'archive'),
+    filename    : isTrial ? `trial_v${version}` : `v${version}`,
+    pathPrefix  : 'dist'
+  }))
+}
+
+
+
+RULES.push({
+  exclude: /svg/,
   test: /\.scss/,
   use: [
-    'style-loader',
+    { loader: 'style-loader' },
     {
       loader: 'css-loader',
-      options: {
-        // 0 => no loaders (default);
-        // 1 => postcss-loader;
-        // 2 => postcss-loader, sass-loader
-        importLoaders: 2
-      },
+      options: { importLoaders: 2 /* postcss-loader, sass-loader */  },
     },
     {
       loader: 'sass-loader',
-      options: {
-        sourceMap: false,
-      }
+      options: { sourceMap: false }
     }
   ],
 })
 
-rules.push({
-  ...common,
+
+RULES.push({
   test: /\.svg$/,
   exclude: /node_modules/,
   include: /svg/,
   loader: 'svg-react-loader',
-  query: {
-  //     classIdPrefix: '[name]-[hash:8]__',
-  //     filters: [
-  //         function (value) {
-  //             // ...
-  //             this.update(newvalue);
-  //         }
-  //     ],
-  //     propsMap: {
-  //         fillRule: 'fill-rule',
-  //         foo: 'bar'
-  //     },
-      xmlnsTest: /^xmlns.*$/
-  }
+  query: { xmlnsTest: /^xmlns.*$/ }
 })
 
-rules.push({
-  ...common,
+
+RULES.push({
+  exclude: /svg/,
   test: /\.tsx?$/,
   loader: 'ts-loader'
 })
+ENTRY.content = './src/content.ts'
+RESOLVE.extensions.push('.ts', '.tsx', '.js')
+RESOLVE.modules.push(path.join(__dirname, 'src/tsx'), 'node_modules')
 
 
 
 
-
-
-const config: webpack.Configuration = {
-  entry: {
-    content: './src/content.ts',
-    // options: './src/options.js',
-    // 'icon-16': './src/icons/icon-16.png'
-  },
+const CONFIG: Webpack.Configuration = {
+  entry: ENTRY,
+  resolve: RESOLVE,
+  plugins: PLUGINS,
+  module: { rules: RULES },
   output: {
     filename: '[name].js',
     path    : path.join(__dirname, 'dist'),
   },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
-    modules: [path.join(__dirname, 'src/tsx'), 'node_modules']
-  },
-  plugins: plugins,
-  module: { rules },
 }
 
-export default config
+export default CONFIG
