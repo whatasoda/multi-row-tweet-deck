e963be1735d6f2cf454c2c5d4d3449670506b926
whatasoda <a16dc107@dhw.ac.jp>
2019-05-20 02:38:35 +0900

    add jss typing

---
+++ types/jss/index.d.ts
@@ -0,0 +1,17 @@
+declare module 'jss-plugin-camel-case' {
+  import { JSSPlugin } from 'jss';
+
+  export default function jssPluginSyntaxCamelCase(): JSSPlugin;
+}
+
+declare module 'jss-plugin-global' {
+  import { JSSPlugin } from 'jss';
+
+  export default function jssPluginSyntaxCamelCase(): JSSPlugin;
+}
+
+declare module 'jss-plugin-nested' {
+  import { JSSPlugin } from 'jss';
+
+  export default function jssPluginSyntaxCamelCase(): JSSPlugin;
+}
