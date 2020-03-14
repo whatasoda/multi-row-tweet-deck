7247c2777aceb7e7f448d4f6f8a85d9ffd358416
whatasoda <git@whatasoda.me>
2020-03-14 17:40:06 +0900

    feat: add store utils

---
+++ src/shared/utils/storeHook.tsx
@@ -0,0 +1,39 @@
+import React, { createContext, useReducer, useCallback, useContext } from 'react';
+import { ActionsRecord, ActionCreatorOf, ReducerOf } from './storeModule';
+
+type ContextInjection<A extends ActionsRecord, S> = [
+  React.Context<S> | undefined,
+  (React.Context<ActionCreatorOf<A>> | undefined)?
+];
+
+export const createStoreHook = <A extends ActionsRecord, S>(
+  createAction: ActionCreatorOf<A>,
+  reducer: ReducerOf<A, S>,
+  ctx?: ContextInjection<A, S>,
+) => {
+  const [
+    stateContext = createContext<S>(null as any),
+    dispatcherContext = createContext<ActionCreatorOf<A>>(null as any),
+  ] = ctx || [];
+
+  const useState = () => useContext(stateContext);
+  const useDispatcher = () => useContext(dispatcherContext);
+  const initialize = (initialState: S): React.FC => {
+    return function Provider({ children }) {
+      const [state, dispatch] = useReducer(reducer, initialState);
+      const dispatcher = useCallback<ActionCreatorOf<A>>((type, ...args) => {
+        const action = createAction(type, ...args);
+        dispatch(action);
+        return action;
+      }, []);
+
+      return (
+        <dispatcherContext.Provider value={dispatcher}>
+          <stateContext.Provider value={state} children={children} />
+        </dispatcherContext.Provider>
+      );
+    };
+  };
+
+  return [initialize, useState, useDispatcher] as const;
+};
