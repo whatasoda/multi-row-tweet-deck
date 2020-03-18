d5dd5b45d24f631fc7ce774936f7c525b674ff79
whatasoda <git@whatasoda.me>
2020-03-18 23:33:08 +0900

    feat(constant): add WEB_EVENT

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -6,3 +6,11 @@ export const HEADER_HEIGHT = {
 
 export const ROOT_CLASS_NAME = 'multi-row';
 export const ROOT_SELECTOR = 'html.multi-row body.multi-row';
+export const EXTENSION_ID = process.env.EXTENSION_ID!;
+
+export const WEB_EVENT = (<T extends string>(inputs: readonly T[]): Record<T, string> => {
+  return inputs.reduce<Record<string, string>>((acc, key) => {
+    acc[key] = `${EXTENSION_ID}:${key}`;
+    return acc;
+  }, {});
+})(['connect'] as const);
