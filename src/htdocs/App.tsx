f970b3d7bf42cf2989aa81db5046c43f4ab9cc8c
whatasoda <git@whatasoda.me>
2020-03-24 23:03:37 +0900

    chore: remove unused pages and routes

--- src/htdocs/App.tsx
+++ src/htdocs/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { CustomThemeProvider } from '../shared/utils/ThemeProvider';
-import { Routes } from './routes';
 import { DragProvider } from './utils/useDrag';
 import { GlobalStyle } from './GlobalStyle';
+import { MultiRowFrame } from './containers/MultiRowFrame';
 
 export const App = () => (
   <CustomThemeProvider defaultTheme="light">
     <>
       <GlobalStyle />
       <DragProvider mode="animationFrame">
-        <Routes />
+        <MultiRowFrame />
       </DragProvider>
     </>
   </CustomThemeProvider>
--- src/htdocs/pages/Config.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { MultiRowFrame } from '../containers/MultiRowFrame';
-
-export const Config = () => {
-  return <MultiRowFrame />;
-};
