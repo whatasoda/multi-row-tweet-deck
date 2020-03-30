894ece9d462e7a0ae838aa673a4a7e5fc099c3b3
whatasoda <git@whatasoda.me>
2020-03-30 20:44:24 +0900

    chore: set zip plugin up

--- scripts/webpack/extension/prod.config.js
+++ scripts/webpack/extension/prod.config.js
@@ -1,7 +1,10 @@
+const webpack = require('webpack');
 const merge = require('webpack-merge');
 const TerserPlugin = require('terser-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const webpack = require('webpack');
+const ZipWebpackPlugin = require('zip-webpack-plugin');
+
+const { npm_package_version } = process.env;
 
 module.exports = merge(require('./base.config'), {
   plugins: [
@@ -11,6 +14,11 @@ module.exports = merge(require('./base.config'), {
       'process.env': {
         NODE_ENV: JSON.stringify('production'),
       },
+    }),
+    new ZipWebpackPlugin({
+      path: '../',
+      filename: `multirow-tweetdeck-v${npm_package_version}.zip`,
+      exclude: [/[\\/]jsons-extension\.js$/],
     }),
   ],
 });
